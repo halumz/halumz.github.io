c2c2923c2d4b7d917b87a911824e05d2e9115435
Mashuk Sadman <mashuksadman@gmail.com>
2018-07-21 11:03:23 +0600

    updated

--- src/config/objectives.js
+++ src/config/objectives.js
@@ -1,6 +1,6 @@
 export default [
   {
     txt:
-      'Seeking a position in a company where I could apply my software engineering experience specially on React JS and logic related problems and contribute to the development process both as an individual and a good team player.'
+      'Seeking a position in a company where I can apply my software engineering experience specially on React JS and logic related problems and contribute to the development process both as an individual and a good team player.'
   }
 ];
